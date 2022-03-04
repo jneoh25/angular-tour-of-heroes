51e1316b282e78b298b56f783da4b3a78a852b7c
jneoh25 <jneoh25@iskl.edu.my>
2022-03-04 17:02:39 +0800

    test

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,7 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = Hero[] = []; 
+  heroes : Hero[] = []; 
   selectedHero?: Hero;
 
   constructor(private heroService: HeroService) { }
